aa62a9233c454b990dfdc53f95306ffa139ba7ee
fahiem152 <amfahiem010502@gmail.com>
2022-10-20 20:36:11 +0700

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,66 +1,45 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Dashboard from "../views/Dashboard.vue";
-import Tables from "../views/Tables.vue";
-import Billing from "../views/Billing.vue";
-import VirtualReality from "../views/VirtualReality.vue";
-import RTL from "../views/Rtl.vue";
-import Profile from "../views/Profile.vue";
-import Signup from "../views/Signup.vue";
-import Signin from "../views/Signin.vue";
+import { certCookies } from "../plugins/cookies";
+import routes from'./routes';
 
-const routes = [
-  {
-    path: "/",
-    name: "/",
-    redirect: "/dashboard-default",
-  },
-  {
-    path: "/dashboard-default",
-    name: "Dashboard",
-    component: Dashboard,
-  },
-  {
-    path: "/tables",
-    name: "Tables",
-    component: Tables,
-  },
-  {
-    path: "/billing",
-    name: "Billing",
-    component: Billing,
-  },
-  {
-    path: "/virtual-reality",
-    name: "Virtual Reality",
-    component: VirtualReality,
-  },
-  {
-    path: "/rtl-page",
-    name: "RTL",
-    component: RTL,
-  },
-  {
-    path: "/profile",
-    name: "Profile",
-    component: Profile,
-  },
-  {
-    path: "/signin",
-    name: "Signin",
-    component: Signin,
-  },
-  {
-    path: "/signup",
-    name: "Signup",
-    component: Signup,
-  },
+// const routes = [
+//   {
+//     path: "/",
+//     name: "/",
+//     redirect: "/dashboard-default",
+//   },
+ 
+//   {
+//     path: "/virtual-reality",
+//     name: "Virtual Reality",
+//     component: VirtualReality,
+//   },
+//   {
+//     path: "/rtl-page",
+//     name: "RTL",
+//     component: RTL,
+//   },
   
-];
+ 
+  
+// ];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-  linkActiveClass: "active",
+  linkActiveClass: "",
+  linkExactActiveClass: "active",
+});
+
+router.beforeEach((to, from, next,)=>{
+  const {id}=certCookies();
+  if(to.matched.some(({meta})=>meta.auth) && !id){
+    next({name: 'Signin'});
+  }else if (to.matched.some(({path})=>path.includes('auth'))&& id) {
+    next({name: 'Default'});
+  }else{
+    next();
+  }
 });
 
 export default router;
