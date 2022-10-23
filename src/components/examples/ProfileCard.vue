8f4853190e496cadddf621bbccc49b86b9826e53
MHabilArifin <mhabilarifin1414@gmail.com>
2022-10-23 20:24:28 +0700

    Merapikan Header dan Profil

--- src/components/examples/ProfileCard.vue
+++ src/components/examples/ProfileCard.vue
@@ -5,10 +5,7 @@
       <div class="col-4 col-lg-4 order-lg-2">
         <div class="mt-n4 mt-lg-n6 mb-4 mb-lg-0">
           <a href="javascript:;">
-            <img
-              src="../../assets/img/team-2.jpg"
-              class="rounded-circle img-fluid border border-2 border-white"
-            />
+            <img src="../../assets/img/team-2.jpg" class="rounded-circle img-fluid border border-2 border-white" />
           </a>
         </div>
       </div>
@@ -19,10 +16,7 @@
         <a href="javascript:;" class="btn btn-sm btn-info mb-0 d-block d-lg-none">
           <i class="ni ni-collection"></i>
         </a>
-        <a
-          href="javascript:;"
-          class="btn btn-sm btn-dark float-right mb-0 d-none d-lg-block"
-        >Message</a>
+        <a href="javascript:;" class="btn btn-sm btn-dark float-right mb-0 d-none d-lg-block">Message</a>
         <a href="javascript:;" class="btn btn-sm btn-dark float-right mb-0 d-block d-lg-none">
           <i class="ni ni-email-83"></i>
         </a>
@@ -49,18 +43,12 @@
       </div>
       <div class="text-center mt-4">
         <h5>
-          Mark Davis
-          <span class="font-weight-light">, 35</span>
+          Kelompok 3
+          <span class="font-weight-light"> Kelas.SW-C</span>
         </h5>
-        <div class="h6 font-weight-300">
-          <i class="ni location_pin mr-2"></i>Bucharest, Romania
-        </div>
-        <div class="h6 mt-4">
-          <i class="ni business_briefcase-24 mr-2"></i>Solution Manager - Creative Tim Officer
-        </div>
-        <div>
-          <i class="ni education_hat mr-2"></i>University of Computer Science
-        </div>
+        <div class="h6 font-weight-300"><i class="ni location_pin mr-2"></i>Indonesia</div>
+        <div class="h6 mt-4"><i class="ni business_briefcase-24 mr-2"></i>Front-End</div>
+        <div><i class="ni education_hat mr-2"></i>Stechoq</div>
       </div>
     </div>
   </div>
